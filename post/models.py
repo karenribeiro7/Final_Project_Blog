f316c8cfa37281911954b3ae9e542788d5e08b29
Danny Uchoa <yaradanieleuchoa@gmail.com>
2024-03-25 20:26:07 -0300

    inicio da criação das categorias

--- post/models.py
+++ post/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+# Definindo o modelo para Post
+class Postagem(models.Model):
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    data_publicacao = models.DateTimeField(default=timezone.now)
+    categoria = models.ForeignKey('Categorias', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+# Definindo o modelo para Categorias
+class Categorias(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
--- post/views.py
+++ post/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Categorias, Postagem
 
 
-# Create your views here.
 def postagem(request):
-    return render(request, 'postagem.html')
+    categorias = Categorias.objects.all()
+    return render(request, 'postagem.html', {'categorias': categorias})
+
+def posts_por_categoria(request, categoria_id):
+    categoria = Categorias.objects.get(id=categoria_id)
+    posts = Postagem.objects.filter(categoria=categoria).order_by('-data_publicação')
+    return render(request, 'posts_por_categoria.html',{'categoria': categoria, 'posts': posts})
