c92760e8741dd6fb1a0678e28c8c4d975aab1364
karenribeiro7 <kevr@discente.ifpe.edu.br>
2024-03-28 20:56:53 -0300

    adicionando primeiras funcionalidades

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Cadastro(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50,unique=True)
+    senha = models.CharField(max_length=50)
+    data = models.DateTimeField(auto_now_add=True)
+    is_super_user= models.BooleanField(default=False)
+
--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\famil\Desktop\Danny Stuffs\Bootcamp\Desafios\Final_Project_Blog\Final_Project_Blog\env
+set VIRTUAL_ENV=D:\Documents\Curso Python e Django\Final_Project_Blog\env
 
 if not defined PROMPT set PROMPT=$P$G
 
