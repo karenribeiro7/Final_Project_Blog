c68f11b9f5e7d2d4f137b40969f6df5ebb185240
Dayanaferrer <dayanaferreira2@gmail.com>
2024-04-08 15:40:54 -0300

    criando vizualizacoes e formulario para cadastrar

--- cadastro/forms.py
+++ cadastro/forms.py
@@ -1,2 +1,11 @@
 from django import forms
+from .models import Cadastro
 
+class CadastroForm(forms.ModelForm):
+    class Meta:
+        model = Cadastro
+        fields = ['nome', 'sobrenome', 'email', 'senha']
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    senha = forms.CharField(widget=forms.PasswordInput)
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Cadastro
+from .forms import CadastroForm, LoginForm
+from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages 
+from django.http import HttpResponse
 
 def cadastro(request):
-    return render(request, 'galeria/cadastro.html')
+    if request.method == 'POST':
+        form = CadastroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Cadastro realizado com sucesso! Faça login para continuar.') 
+            return redirect('login')
+    else:
+        form = CadastroForm()   
+    return render(request, 'galeria/cadastro.html', {'form': form})
 
 def login(request):
-    return render(request, 'galeria/login.html')
+   if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            senha = form.cleaned_data.get('senha')
+            user = authenticate(request, email=email, password=senha)
+            if user is not None:
+                login(request, user)
+                return redirect('paineldousuario.html')
+        else:
+            return HttpResponse("Usuária não cadastrada!")
+        
+def logout (request):
+    logout(request)
+    return redirect('galeria/inicio.html')
 
