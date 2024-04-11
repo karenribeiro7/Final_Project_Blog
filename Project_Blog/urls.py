6614f3f4d140cbd2cb223e381a06dbafd18dc2fd
Dayanaferrer <dayanaferreira2@gmail.com>
2024-04-11 16:11:07 -0300

    ajustando o cadastro para salvar no admin django

--- Project_Blog/urls.py
+++ Project_Blog/urls.py
@@ -12,15 +12,14 @@ router = SimpleRouter(trailing_slash=False)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio , name='inicio'), #ok
-    path('cadastro/', cadastro_user, name='cadastrar'), # ok
-    path('login/', fazer_login , name='login'), #OK
-    #path('usuario/', usuario),  #OK   
+    path('', inicio , name='inicio'), 
+     path('cadastro/', cadastro_user, name='cadastro'),
+    path('login/', fazer_login , name='login'), 
     path('sobre/', sobre, name='sobre'), #OK
     path('contato/', contato, name='contato'), #OK
     path('post/', Post, name='post'), #OK
     path('paineldousuario/', paineldousuario, name='paineldousuario'), #OK
-    path('usuario/', include('post.urls')), #Acrescentado para visualizar as páginas de criar e editar postagem
+    #path('usuario/', include('post.urls')), #Acrescentado para visualizar as páginas de criar e editar postagem
     path('gerenciar_perfil/', gerenciar_perfil, name='gerenciar_perfil'), #OK
 ]
 urlpatterns += router.urls
