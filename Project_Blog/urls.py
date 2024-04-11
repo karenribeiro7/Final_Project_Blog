3e34dcf1b4825a626b8563ece962e3eaaa4256f4
Dayanaferrer <dayanaferreira2@gmail.com>
2024-04-11 15:04:59 -0300

    correcao da altenticacao do usuario pra salvar no django admin | excluir o app cadastro para utilizar apenas o app usuario

--- Project_Blog/urls.py
+++ Project_Blog/urls.py
@@ -1,21 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-from cadastro.views import cadastroModelViewSet
 from django.conf import settings
 from django.conf.urls.static import static
-from cadastro.views import cadastrar, login, paineldousuario, gerenciar_perfil
+from usuario.views import cadastro_user, fazer_login, paineldousuario, gerenciar_perfil
 from inicio.views import inicio, sobre, contato
 from post.views import Post
 
 router = SimpleRouter(trailing_slash=False)
-router.register('cadastro', cadastroModelViewSet)
+#router.register('cadastro', cadastroModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', inicio , name='inicio'), #ok
-    path('cadastro/', cadastrar, name='cadastrar'), # ok
-    path('login/', login , name='login'), #OK
+    path('cadastro/', cadastro_user, name='cadastrar'), # ok
+    path('login/', fazer_login , name='login'), #OK
     #path('usuario/', usuario),  #OK   
     path('sobre/', sobre, name='sobre'), #OK
     path('contato/', contato, name='contato'), #OK
---
+++ usuario/views.py
@@ -0,0 +1,63 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.contrib.auth import logout
+from usuario.forms import CadastroForm, LoginForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+#from django.contrib.auth.decorators import login_required
+#from django.contrib.auth.forms import AuthenticationForm
+#from django.contrib.auth.models import User
+
+def logout_user(request):
+    logout(request)
+    return redirect('inicio')
+
+def paineldousuario(request):
+    usuario = request.user
+    dados_usuario = {}
+    if usuario.is_authenticated:
+        dados_usuario = {
+            'nome': usuario.username,
+            'email': usuario.email,
+        }
+    return render(request, 'galeria/paineldousuario.html', {'usuario': dados_usuario})
+
+def cadastro_user(request):
+    if request.method == 'POST':
+        form = CadastroForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('paineldousuario')
+    else:
+        form = CadastroForm()
+    return render(request, 'galeria/cadastro.html', {'form': form})
+
+def fazer_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request, request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('paineldousuario')  
+    else:
+        form = LoginForm()
+    return render(request, 'galeria/login.html', {'form': form})
+
+    
+def gerenciar_perfil(request):
+    usuario = request.user
+    if usuario.is_authenticated:
+        if request.method == 'POST':
+            email = request.POST.get('email')
+            usuario.email = email
+            usuario.save()
+            messages.success(request, 'Perfil atualizado com sucesso')
+    return render(request, 'galeria/gerenciar_perfil.html', {'usuario': usuario})
